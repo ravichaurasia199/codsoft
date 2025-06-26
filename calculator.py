4de61a05f3d5a05408432c72787c761a6730a3d8
ravichaurasia199 <ravichaurasiya8797@gmail.com>
2025-06-26 21:54:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,47 @@
+def add(x, y):
+     return x+y
+
+def subtract(x,y):
+     return x-y
+
+def multiply(x,y):
+     return x*y
+
+def divide(x,y):
+     if y == 0 :
+          return "Error! Division by zero."
+     return x/y
+
+print("Selct operation:")
+print("1. Add")
+print("2. Substract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter your choice (1/2/3/4): ")
+
+if choice in ('1','2','3','4'):
+    try:
+        num1 = float(input("Enter first number:"))
+        num2 = float(input("Enter second number:"))
+    except ValueError:
+         print("Invalid input! Please enter numeric values")
+         exit()
+
+    if choice =='1':
+         print("Result:", add(num1,num2))
+
+    elif choice == '2':
+         print("Result:", subtract(num1, num2))
+
+    elif choice == '3':
+         print("Result:", multiply(num1, num2))
+
+
+    elif choice == '4':
+         print("Result:", divide(num1, num2))
+
+    else:
+         print("Invalid choice")
+          
+          
